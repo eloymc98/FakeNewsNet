7ff93fa8ef62a490c6146deaf4a57038cad96f7b
Alejandro Tejada Lapuerta <alejandro.tejada@bsc.es>
2022-01-06 00:50:02 +0100

    new implementation

--- code/data_parsing.py
+++ code/data_parsing.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-ids_dict = {} #dictionary to compress the data
+ids_dict = {}  # dictionary to compress the data
 
 def parsing_followers():
 
@@ -15,15 +15,22 @@ def parsing_followers():
         json_object = json.load(open(filename))
 
         id = json_object["user_id"]
+        if len(ids_dict) == 0:
+            ids_dict[id] = 1
+        if id not in ids_dict.keys():
+            max_value = max(ids_dict.values())
+            ids_dict[id] = max_value + 1
         followers = json_object["followers"]
 
         with open('graph.txt', 'a+') as f:
             for user in followers:
-                f.write(str(id) + " " + str(user) + "\n")
+                if user not in ids_dict.keys():
+                    max_value = max(ids_dict.values())
+                    ids_dict[user] = max_value+1
+                f.write(str(ids_dict[id]) + " " + str(ids_dict[user]) + "\n")
 
 
 def parsing_following():
-
     fileList = []
 
     for filenames in os.walk('fakenewsnet_dataset/user_following'):
@@ -34,11 +41,19 @@ def parsing_following():
         json_object = json.load(open(filename))
 
         id = json_object["user_id"]
+        if len(ids_dict) == 0:
+            ids_dict[id] = 1
+        if id not in ids_dict.keys():
+            max_value = max(ids_dict.values())
+            ids_dict[user] = max_value + 1
         followers = json_object["following"]
 
         with open('graph.txt', 'a+') as f:
             for user in followers:
-                f.write(str(id) + " " + str(user) + "\n")
+                if user not in ids_dict.keys():
+                    max_value = max(ids_dict.values())
+                    ids_dict[user] = max_value + 1
+                f.write(str(ids_dict[id]) + " " + str(ids_dict[user]) + "\n")
 
 parsing_followers()
 parsing_following()
