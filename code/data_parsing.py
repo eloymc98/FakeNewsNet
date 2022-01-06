70f8757a6474045f29bfaa18f7012ed0fbed27fb
Alejandro Tejada Lapuerta <alejandro.tejada@bsc.es>
2022-01-06 12:26:10 +0100

    graph and infected

--- code/data_parsing.py
+++ code/data_parsing.py
@@ -2,24 +2,25 @@ import json
 import os
 
 ids_dict = {}  # dictionary to compress the data
+# The key is the id, the value is a number from 1 to n
 
 def parsing_followers():
 
     fileList = []
 
-    for filenames in os.walk('fakenewsnet_dataset/user_followers'):
+    for filenames in os.walk('fakenewsnet_dataset/user_followers'): # collect files names
         fileList.append(filenames)
 
-    for file in fileList[0][2]:
+    for file in fileList[0][2]: # files stored in 0,2, don't ask me why
         filename = 'fakenewsnet_dataset/user_followers/{}'.format(file)
         json_object = json.load(open(filename))
 
         id = json_object["user_id"]
-        if len(ids_dict) == 0:
+        if len(ids_dict) == 0: # if dictionary empty, start it
             ids_dict[id] = 1
         if id not in ids_dict.keys():
-            max_value = max(ids_dict.values())
-            ids_dict[id] = max_value + 1
+            max_value = max(ids_dict.values())  # find max value
+            ids_dict[id] = max_value + 1 # value = max + 1
         followers = json_object["followers"]
 
         with open('graph.txt', 'a+') as f:
@@ -45,7 +46,7 @@ def parsing_following():
             ids_dict[id] = 1
         if id not in ids_dict.keys():
             max_value = max(ids_dict.values())
-            ids_dict[user] = max_value + 1
+            ids_dict[id] = max_value + 1
         followers = json_object["following"]
 
         with open('graph.txt', 'a+') as f:
@@ -55,6 +56,37 @@ def parsing_following():
                     ids_dict[user] = max_value + 1
                 f.write(str(ids_dict[id]) + " " + str(ids_dict[user]) + "\n")
 
+
+def parsing_tweets():
+    fileList = []
+    tweetsList = []
+    infected = []
+
+    for filenames in os.walk('fakenewsnet_dataset/politifact/fake_subset'):
+        fileList.append(filenames)
+
+    for file in fileList[0][1]:
+        for tweetFile in os.walk('fakenewsnet_dataset/politifact/fake_subset/{}/tweets'.format(file)):
+            tweetsList.append(tweetFile)
+
+        for tweet in tweetsList[0][2]:
+            filename = 'fakenewsnet_dataset/politifact/fake_subset/{}/tweets/{}'.format(file,tweet)
+            json_object = json.load(open(filename))
+
+            id = json_object["user"]["id"]
+            if len(ids_dict) == 0:
+                ids_dict[id] = 1
+            if id not in ids_dict.keys():
+                max_value = max(ids_dict.values())
+                ids_dict[id] = max_value + 1
+            infected.append(ids_dict[id])
+
+        with open('infected.txt', 'w') as f:
+            for compressed_id in infected:
+                    f.write(str(compressed_id) + "\n")
+
+
 parsing_followers()
 parsing_following()
+parsing_tweets()
 
